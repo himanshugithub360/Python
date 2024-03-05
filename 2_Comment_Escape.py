4ceff5d4c3b52fe46822a60f6e5c07a6dc2b593d
himanshugithub360 <himanshuramgariya@gmail.com>
2024-03-05 19:54:20 +0530

    Day 2 - Comment and Escape

---
+++ 2_Comment_Escape.py
@@ -0,0 +1,38 @@
+#         comment in python
+ #      ----------------------
+# for single , multile line use # , ctrl+/ , ''' '''',""" """
+
+print("hi Himanshu")
+
+"""This is comment line ,not printed in output"""
+# now comment this line 
+
+#         Escape statment in python
+#        -----------------------------
+#  \n for break line
+#  \ followed by anything will be printed in output
+#  eg; \" ---> "
+
+print("hey I am Himanshu and that one is also good one")
+     # print("hey I am Himanshu 
+      #       and that one is also good one")         ---> this will create problem . Not used for making new line
+print("hey I am Himanshu \nand that one is also good one")
+print("hey I am \"Himanshu\" and that one is also good one")
+print('hey I am \'Himanshu\" and that one is also good one')
+
+
+
+
+print("Hey",6,7)
+
+# for joining the output use sep="anything"
+print("hey",3,4,sep="@")
+
+
+print("hey",3,4,sep="@")
+print("Himanshu")
+
+# for joning the two string we use also end="anything"
+print("hey",3,4,sep="@", end="%")
+print("Himanshu")
+
