659dc4acdf61f36c56cad1ecd3575e4e23483415
himanshugithub360 <himanshuramgariya@gmail.com>
2024-03-08 00:28:13 +0530

    Day 4 Adding operators

---
+++ 4_Operators.py
@@ -0,0 +1,22 @@
+a = 25
+b = 5
+print("Addition of ",a," + ",b,"is ", a+b)
+print("Subtraction of ",a," - ",b,"is ", a-b)
+print("Multiplicaton of ",a," * ",b,"is ", a*b)
+print("Division of ",a," / ",b,"is ", a/b)
+print("Exponential of ",a," ** ",b,"is ", a**b)
+print("Modulus of ",a," % ",b,"is ", a%b)   #Remainder
+print("Floor Division of ",a," // ",b,"is ", a//b) #Quotient
+
+print("\n")
+
+m = 17
+n = 5
+print("Addition of ",m," + ",n,"is ", m+n)
+print("Subtraction of ",m," - ",n,"is ", m-n)
+print("Multiplicaton of ",m," * ",n,"is ", m*n)
+print("Division of ",m," / ",n,"is ", m/n)
+print("Exponential of ",m," ** ",n,"is ", m**n)
+print("Modulus of ",m," % ",n,"is ",m%n)
+print("Floor Division of ",m," // ",n,"is ", m//n)
+
