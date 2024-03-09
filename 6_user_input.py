946b81c1c9c764e277a80870eb2c65a46efde906
himanshugithub360 <himanshuramgariya@gmail.com>
2024-03-09 22:45:07 +0530

    Day 6 Learn User input

---
+++ 6_user_input.py
@@ -0,0 +1,14 @@
+a = input("Enter the name: ")
+print("My name is ",a)
+
+x = input ("Enter the first number : ")
+y = input("Enter the second number : ")
+print(x+y)  #-----> by default input is taken as STRING, for making into number we have to use typecasting
+
+print(int(x) + int(y))
+
+#Another way of Typecasting at input time
+m = int(input ("Enter the first number : "))
+n = float(input("Enter the second number : "))
+print(m+n)
+
