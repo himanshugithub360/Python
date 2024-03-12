e9b978c8c20efab8d5335aeb4f640ea598e1ac5d
himanshugithub360 <himanshuramgariya@gmail.com>
2024-03-12 22:54:56 +0530

    Day 6 Learn String

---
+++ 7_String.py
@@ -0,0 +1,41 @@
+name = "Himanshu"
+friend = "Madhav"
+AnotherFriend = 'Ayush'
+
+print(name)
+print("Namaste 🙏 ",friend)
+print("Namaste 🙏 " + friend)
+
+print(friend,"and",AnotherFriend)
+
+apple = """dlskjfgk;gkdl;ggdgjbk
+dkjfdklgjdkjdlefe
+kdjglkfkg;fkllgflhf;k;fhk;
+uhfkjdlsgr;oeip5orltr5r"""
+
+print(apple)
+
+"""this is comment line"""
+
+# hi = "iuhsdkg
+# sdkjgklfjfbb"    this will show error because multiline is not allowed in single qote
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[7])
+# print(name[8])  ---->this will show error beacus we referencing ou of index
+
+print('LET USE A FOR LOOP FOR PRINTING CHARECTOR BY CHARECTOR ')
+for character in name:
+    print(character)
+
+
+
+for hi in apple:
+    print(hi)    
+
+
